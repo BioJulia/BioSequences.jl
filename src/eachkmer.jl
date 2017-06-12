ea12be5f1c0abf10cb8e211d350f9f8b2b2bfc23
Ben J. Ward <ward9250@gmail.com>
2017-06-12 23:30:09 +0100

    eachkmer alterations

--- src/eachkmer.jl
+++ src/eachkmer.jl
@@ -75,7 +75,7 @@ end
     k = kmersize(T)
     pos, kmer = state
     pos += it.step
-    has_ambiguous = false
+    ambig_or_gap = false
 
     # faster path: return the next overlapping kmer if possible
     if it.step < k && pos + k - 1 ≤ endof(it.seq)
@@ -83,19 +83,19 @@ end
         if it.step == 1
             nt = inbounds_getindex(it.seq, pos+offset)
             kmer = kmer << 2 | trailing_zeros(nt)
-            has_ambiguous |= isambiguous(nt)
+            ambig_or_gap |= (isambiguous(nt) || isgap(nt))
         else
             for i in 1:it.step
                 nt = inbounds_getindex(it.seq, pos+i-1+offset)
                 kmer = kmer << 2 | trailing_zeros(nt)
-                has_ambiguous |= isambiguous(nt)
+                ambig_or_gap |= (isambiguous(nt) || isgap(nt))
             end
         end
-        if !has_ambiguous
+        if !ambig_or_gap
             return (state[1], convert(T, state[2])), (pos, kmer)
         end
     end
-    
+
     # fallback
     while pos + k - 1 ≤ endof(it.seq)
         kmer, ok = extract_kmer_impl(it.seq, pos, k)
@@ -109,11 +109,11 @@ end
 
 function extract_kmer_impl(seq, from, k)
     kmer::UInt64 = 0
-    has_ambiguous = false
+    ambig_or_gap = false
     for i in 1:k
         nt = inbounds_getindex(seq, from+i-1)
         kmer = kmer << 2 | trailing_zeros(nt)
-        has_ambiguous |= isambiguous(nt)
+        ambig_or_gap |= (isambiguous(nt) || isgap(nt))
     end
-    return kmer, !has_ambiguous
+    return kmer, !ambig_or_gap
 end
