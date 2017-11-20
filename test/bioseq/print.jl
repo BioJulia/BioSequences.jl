e9424652eb0d7d34bb9b595fa131ba69d3a569bf
Ben J. Ward <ward9250@gmail.com>
2017-11-20 18:22:35 +0000

    Added tests for altered show methods

--- test/bioseq/print.jl
+++ test/bioseq/print.jl
@@ -1,21 +1,28 @@
 @testset "Print" begin
     buf = IOBuffer()
+
     print(buf, dna"")
     @test String(take!(buf)) == ""
 
-    buf = IOBuffer()
     print(buf, dna"ACGTN")
     @test String(take!(buf)) == "ACGTN"
 
-    buf = IOBuffer()
     print(buf, rna"ACGUN")
     @test String(take!(buf)) == "ACGUN"
 
-    buf = IOBuffer()
     print(buf, dna"A"^100)
     @test String(take!(buf)) == "A"^100
 
-    buf = IOBuffer()
     print(buf, dna"A"^100, width=70)
     @test String(take!(buf)) == string("A"^70, '\n', "A"^30)
+end
+
+@testset "Show" begin
+    buf = IOBuffer()
+
+    show(buf, dna"")
+    @test String(take!(buf)) == "0nt DNA Sequence:\n< EMPTY SEQUENCE >"
+
+    show(buf, dna"ATCG")
+    @test String(take!(buf)) == "4nt DNA Sequence:\nATCG"
 end
--- test/kmers/print.jl
+++ test/kmers/print.jl
@@ -1,13 +1,22 @@
 @testset "Print" begin
     buf = IOBuffer()
+
     print(buf, DNAKmer(""))
     @test String(take!(buf)) == ""
 
-    buf = IOBuffer()
     print(buf, DNAKmer("ACGT"))
     @test String(take!(buf)) == "ACGT"
 
-    buf = IOBuffer()
     print(buf, RNAKmer("ACGU"))
     @test String(take!(buf)) == "ACGU"
+end
+
+@testset "Show" begin
+    buf = IOBuffer()
+
+    show(buf, DNAKmer(""))
+    @test String(take!(buf)) == "DNA 0-mer:\n< EMPTY SEQUENCE >"
+
+    show(buf, DNAKmer("AGAGT"))
+    @test String(take!(buf)) == "DNA 5-mer:\nAGAGT"
 end
