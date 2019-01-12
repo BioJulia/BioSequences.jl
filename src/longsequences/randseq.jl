233722c8de3a41da268e4ea23e88c5316ad4c01e
jakobnissen <jakobnybonissen@gmail.com>
2019-01-12 18:38:51 +0100

    Fixed new random sequence interface and added tests Fixed some docstrings All random sequence methods now take an optional AbstractRNG as first argument Disallow construction of RandomUniform with no elements Allow construction of RandomWeighted with only one element Added tests for all methods in randseq.jl

--- src/longsequences/randseq.jl
+++ src/longsequences/randseq.jl
@@ -6,7 +6,7 @@
 # This file is a part of BioJulia.
 # License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE.md
 
-import Random: Sampler
+import Random: Sampler, GLOBAL_RNG
 
 """
     SamplerUniform{T}
@@ -24,12 +24,17 @@ struct SamplerUniform{T} <: Sampler{T}
 
     function SamplerUniform{T}(elems) where {T}
         elemsvector = convert(Vector{T}, collect(elems))
+        if length(elemsvector) < 1
+            throw(ArgumentError("elements collection must be non-empty"))
+        end
         return new(elemsvector)
     end
 end
 
 SamplerUniform(elems) = SamplerUniform{eltype(elems)}(elems)
-Base.rand(rng::AbstractRNG, sp::SamplerUniform) = rand(sp.elems)
+Base.eltype(::Type{SamplerUniform{T}}) where {T} = T
+Base.rand(rng::AbstractRNG, sp::SamplerUniform) = rand(rng, sp.elems)
+Base.rand(sp::SamplerUniform) = rand(GLOBAL_RNG, sp.elems)
 const DefaultAASampler = SamplerUniform(aa"ACDEFGHIKLMNPQRSTVWY")
 
 """
@@ -50,26 +55,29 @@ struct SamplerWeighted{T} <: Sampler{T}
     probs::Vector{Float64}
 
     function SamplerWeighted{T}(elems, probs) where {T}
-        probsum = sum(probs)
-        if probsum ≥ 1.0
-            throw(ArgumentError("sum of probabilties cannot exceed 1.0"))
-        elseif length(elems) != length(probs) + 1
+        elemsvector = convert(Vector{T}, collect(elems))
+        probsvector = convert(Vector{Float64}, collect(probs))
+        if !isempty(probsvector)
+            probsum = sum(probsvector)
+            if probsum > 1.0
+                throw(ArgumentError("sum of probabilties cannot exceed 1.0"))
+            elseif minimum(probsvector) < 0.0
+                throw(ArgumentError("probabilities must be non-negative"))
+            end
+        else
+            probsum = 0.0
+        end
+        if length(elemsvector) != length(probsvector) + 1
             throw(ArgumentError("length of elems must be length of probs + 1"))
-        elseif minimum(probs) < 0.0
-            throw(ArgumentError("probabilities must be non-negative"))
         end
         # Even with float weirdness,  we can guarantee x + (1.0 - x) == 1.0,
         # when 0 ≤ x ≤ 1, as there's no exponent, and it works like int addition
-        elemsvector = convert(Vector{T}, collect(elems))
-        probsvector = push!(convert(Vector{Float64}, collect(probs)), 1.0 - probsum)
-        return new(elemsvector, probsvector)
+        return new(elemsvector, push!(probsvector, 1.0 - probsum))
     end
 end
 
 SamplerWeighted(elems, probs) = SamplerWeighted{eltype(elems)}(elems, probs)
-
 Base.eltype(::Type{SamplerWeighted{T}}) where {T} = T
-Base.eltype(::Type{SamplerUniform{T}}) where {T} = T
 
 function Base.rand(rng::AbstractRNG, sp::SamplerWeighted)
     r = rand(rng)
@@ -81,9 +89,10 @@ function Base.rand(rng::AbstractRNG, sp::SamplerWeighted)
     end
     return @inbounds sp.elems[j]
 end
+Base.rand(sp::SamplerWeighted) = rand(GLOBAL_RNG, sp)
 
 """
-    randseq{A::Alphabet, sp::Sampler, len::Integer)
+    randseq([rng::AbstractRNG], A::Alphabet, sp::Sampler, len::Integer)
 
 Generate a LongSequence{A} of length `len` with elements drawn from
 the given sampler.
@@ -97,17 +106,17 @@ julia> seq = randseq(RNAAlphabet{4}(), sp, 50)
 CUNGGGCCCGGGNAAACGUGGUACACCCUGUUAAUAUCAACNNGCGCUNU
 ```
 """
-function randseq(A::Alphabet, sp::Sampler, len::Integer)
+function randseq(rng::AbstractRNG, A::Alphabet, sp::Sampler, len::Integer)
     seq = LongSequence{typeof(A)}(len)
     @inbounds for i in 1:len
-        letter = rand(sp)
+        letter = rand(rng, sp)
         unsafe_setindex!(seq, letter, i)
     end
     return seq
 end
-
+randseq(A::Alphabet, sp::Sampler, len::Integer) = randseq(GLOBAL_RNG, A, sp, len)
 """
-    randseq{A::Alphabet, len::Integer}
+    randseq([rng::AbstractRNG], A::Alphabet, len::Integer)
 
 Generate a LongSequence{A} of length `len` from the specified alphabet, drawn
 from the default distribution. User-defined alphabets should implement this
@@ -115,7 +124,7 @@ method to implement random LongSequence generation.
 
 For RNA and DNA alphabets, the default distribution is uniform across A, C, G,
 and T/U.
-For AminoAcidAlphabet, it is uniform across the 20 proteogenic amino acids.
+For AminoAcidAlphabet, it is uniform across the 20 standard amino acids.
 For a user-defined alphabet A, default is uniform across all elements of
 `symbols(A)`.
 
@@ -126,7 +135,7 @@ julia> seq = randseq(AminoAcidAlphabet(), 50)
 VFMHSIRMIRLMVHRSWKMHSARHVNFIRCQDKKWKSADGIYTDICKYSM
 ```
 """
-function randseq(A::NucleicAcidAlphabet{4}, len::Integer)
+function randseq(rng::AbstractRNG, A::NucleicAcidAlphabet{4}, len::Integer)
     seq = LongSequence{typeof(A)}(len)
     randombits = zero(UInt64)
     # Generate random bases for each of the UInt64 in the .data field:
@@ -134,7 +143,7 @@ function randseq(A::NucleicAcidAlphabet{4}, len::Integer)
         x = zero(UInt64)
         # 64 bits of randomness is enough for 2x16 4-bit nucleotides
         if isodd(i)
-            randombits = rand(UInt64)
+            randombits = rand(rng, UInt64)
         end
         # Fill each of the 16 bases in the UInt64
         for nucleotide in 1:16
@@ -149,47 +158,54 @@ function randseq(A::NucleicAcidAlphabet{4}, len::Integer)
     end
     return seq
 end
+randseq(A::NucleicAcidAlphabet{4}, len::Integer) = randseq(GLOBAL_RNG, A, len)
 
-function randseq(::AminoAcidAlphabet, len::Integer)
-    return randseq(AminoAcidAlphabet(), DefaultAASampler, len)
+function randseq(rng::AbstractRNG, ::AminoAcidAlphabet, len::Integer)
+    return randseq(rng, AminoAcidAlphabet(), DefaultAASampler, len)
 end
+randseq(A::AminoAcidAlphabet, len::Integer) = randseq(GLOBAL_RNG, A, len)
 
 # Generic fallback method.
-function randseq(A::Alphabet, len::Integer)
+function randseq(rng::AbstractRNG, A::Alphabet, len::Integer)
     if length(A) == 1 << bits_per_symbol(A)
-        return randseq_allbits(A, len)
+        return randseq_allbits(rng, A, len)
     else
         letters = symbols(A)
         sampler = SamplerUniform{eltype(A)}(letters)
-        return randseq(A, sampler, len)
+        return randseq(rng, A, sampler, len)
     end
 end
+randseq(A::Alphabet, len::Integer) = randseq(GLOBAL_RNG, A, len)
 
-function randseq_allbits(A::Alphabet, len::Integer)
-    vector = rand(UInt64, seq_data_len(typeof(A), len))
+function randseq_allbits(rng::AbstractRNG, A::Alphabet, len::Integer)
+    vector = rand(rng, UInt64, seq_data_len(typeof(A), len))
     return LongSequence{typeof(A)}(vector, 1:len, false)
 end
+randseq_allbits(A::Alphabet, len::Integer) = randseq_allbits(GLOBAL_RNG, A, len)
 
 """
-    randdnaseq(len::Integer)
+    randdnaseq([rng::AbstractRNG], len::Integer)
 
 Generate a random LongSequence{DNAAlphabet{4}} sequence of length `len`,
 with bases sampled uniformly from [A, C, G, T]
 """
-randdnaseq(len::Integer) = randseq(DNAAlphabet{4}(), len)
+randdnaseq(rng::AbstractRNG, len::Integer) = randseq(rng, DNAAlphabet{4}(), len)
+randdnaseq(len::Integer) = randseq(GLOBAL_RNG, DNAAlphabet{4}(), len)
 
 """
-    randrnaseq(len::Integer)
+    randrnaseq([rng::AbstractRNG], len::Integer)
 
 Generate a random LongSequence{RNAAlphabet{4}} sequence of length `len`,
 with bases sampled uniformly from [A, C, G, U]
 """
-randrnaseq(len::Integer) = randseq(RNAAlphabet{4}(), len)
+randrnaseq(rng::AbstractRNG, len::Integer) = randseq(rng, RNAAlphabet{4}(), len)
+randrnaseq(len::Integer) = randseq(GLOBAL_RNG, RNAAlphabet{4}(), len)
 
 """
-    randaaseq(len::Integer)
+    randaaseq([rng::AbstractRNG], len::Integer)
 
 Generate a random LongSequence{AminoAcidAlphabet} sequence of length `len`,
 with amino acids sampled uniformly from the 20 standard amino acids.
 """
-randaaseq(len::Integer) = randseq(AminoAcidAlphabet(), len)
+randaaseq(rng::AbstractRNG, len::Integer) = randseq(rng, AminoAcidAlphabet(), len)
+randaaseq(len::Integer) = randseq(GLOBAL_RNG, AminoAcidAlphabet(), len)
