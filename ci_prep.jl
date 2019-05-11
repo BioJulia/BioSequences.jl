d0aae9a778bb843cde5fedf682c39f7b98a68307
Ben J. Ward <benjward@protonmail.com>
2019-05-11 21:14:01 +0100

    Add coverage folder and update travis file

---
+++ ci_prep.jl
@@ -0,0 +1,3 @@
+using Pkg.Registry
+Registry.add(Registry.RegistrySpec(url = "https://github.com/JuliaRegistries/General.git"))
+Registry.add(Registry.RegistrySpec(url = "https://github.com/BioJulia/BioJuliaRegistry.git"))
