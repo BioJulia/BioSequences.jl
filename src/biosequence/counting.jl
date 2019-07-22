53f140b67ea8bf1413127cc456885cd77974ddbb
Ben J. Ward <benjward@protonmail.com>
2019-07-22 21:29:13 +0100

    Move counting to its own file

---
+++ src/biosequence/counting.jl
@@ -0,0 +1,38 @@
+###
+### Counting
+###
+### Counting operations on biological sequence types.
+###
+### This file is a part of BioJulia.
+### License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE.md
+
+"""
+Count how many positions in a sequence satisfy a condition (i.e. f(seq[i]) -> true).
+
+The first argument should be a function which accepts an element of the sequence
+as its first parameter, additional arguments may be passed with `args...`.
+"""
+function Base.count(f::Function, seq::BioSequence, args...)
+    n = 0
+    @inbounds for x in seq
+        if f(x, args...)
+            n += 1
+        end
+    end
+    return n
+end
+
+###
+### GC content
+###
+
+"""
+    gc_content(seq::BioSequence)
+
+Calculate GC content of `seq`.
+"""
+gc_content(seq::NucleotideSeq) = isempty(seq) ? 0.0 : count_gc(seq) / length(seq)
+
+function count_gc(seq::BioSequence)
+    return count(isGC, seq)
+end
