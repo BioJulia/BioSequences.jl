4f2a9b9ae7d73cfc45aae280bff93b38f700e78f
Thomas A. Poulsen <thomas@lha66.dk>
2020-10-31 14:50:51 +0100

    Allow hamming circle around N

--- test/demultiplexer.jl
+++ test/demultiplexer.jl
@@ -118,4 +118,13 @@
         @test n_ok / 10_000 > 0.99
         @test n_ok < n_ok_with_fallback
     end
+
+    @testset "Hamming circle" begin
+        @test sort(BioSequences.hamming_circle(dna"A",1)) == [dna"C",dna"G",dna"T",dna"N"]
+        @test sort(BioSequences.hamming_circle(dna"N",1)) == [dna"A",dna"C",dna"G",dna"T"]
+    end
+    @testset "Levenshtein circle" begin
+        @test sort(BioSequences.levenshtein_circle(dna"A",1)) == [dna"", dna"C", dna"CA", dna"G", dna"GA", dna"T", dna"TA", dna"N", dna"NA"]
+        @test sort(BioSequences.levenshtein_circle(dna"N",1)) == [dna"", dna"A", dna"AN", dna"C", dna"CN", dna"G", dna"GN", dna"T", dna"TN"]
+    end
 end
