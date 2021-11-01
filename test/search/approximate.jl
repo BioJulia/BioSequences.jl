5975bc4e3d568a007ead0919ad200b66358a8ec0
Ben J. Ward <benjward@protonmail.com>
2021-11-01 13:14:58 +0000

    fixup

--- test/search/approximate.jl
+++ test/search/approximate.jl
@@ -18,8 +18,8 @@
         @test findnext(ApproximateSearchQuery(dna"ACG", 0), seq, 1) === 1:3
         @test findnext(ApproximateSearchQuery(dna"ACG", 1), seq, 1) === 1:2
         @test findnext(ApproximateSearchQuery(dna"ACG", 2), seq, 1) === 1:1
-        @test findnext(ApproximateSearchQuery(dna"ACG", 3), seq, 1) === 1:0
-        @test findnext(ApproximateSearchQuery(dna"ACG", 4), seq, 1) === 1:0
+        @test findnext(ApproximateSearchQuery(dna"ACG", 3), seq, 1) === nothing #1:0
+        @test findnext(ApproximateSearchQuery(dna"ACG", 4), seq, 1) === nothing #1:0
     end
 
     @testset "backward" begin
@@ -42,7 +42,7 @@
         @test findprev(ApproximateSearchQuery(dna"ACG", 0), seq, 7) === 5:7
         @test findprev(ApproximateSearchQuery(dna"ACG", 1), seq, 7) === 6:7
         @test findprev(ApproximateSearchQuery(dna"ACG", 2), seq, 7) === 7:7
-        @test findprev(ApproximateSearchQuery(dna"ACG", 3), seq, 7) === 7:6
-        @test findprev(ApproximateSearchQuery(dna"ACG", 4), seq, 7) === 7:6
+        @test findprev(ApproximateSearchQuery(dna"ACG", 3), seq, 7) === nothing #7:6
+        @test findprev(ApproximateSearchQuery(dna"ACG", 4), seq, 7) === nothing #7:6
     end
 end
