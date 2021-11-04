e8a9fbf79040ad5cfbc435087a1fa9638cda6e66
Ben J. Ward <benjward@protonmail.com>
2021-11-04 11:56:11 +0000

    Code refactor

--- src/search/MatchSearchQuery.jl
+++ src/search/MatchSearchQuery.jl
@@ -9,15 +9,16 @@
 
 abstract type ExactSearchQuery{S<:BioSequence} end
 
-function checkeltype(seq1, seq2)
+@inline function checkeltype(seq1::BioSequence, seq2::BioSequence)
     if eltype(seq1) != eltype(seq2)
         throw(ArgumentError("the element type of two sequences must match"))
     end
 end
 
-@inline function check_bloom_mask(q::ExactSearchQuery, x)
-    return bloom_mask(q) & bloom_bits(typeof(q), x) == 0
+@inline function _check_bloom_mask(q::ExactSearchQuery, x::BioSymbol)
+    return _bloom_mask(q) & _bloom_bits(typeof(q), x) == 0
 end
+
 
 """
     findnext(query::ExactSearchQuery, seq::BioSequence, start::Integer)
@@ -37,6 +38,7 @@ function Base.findnext(query::ExactSearchQuery, seq::BioSequence, start::Integer
 end
 
 Base.findfirst(pat::ExactSearchQuery, seq::BioSequence) = findnext(pat, seq, firstindex(seq))
+
 
 """
     findprev(query::ExactSearchQuery, seq::BioSequence, start::Integer)
@@ -58,6 +60,14 @@ Base.findlast(query::ExactSearchQuery, seq::BioSequence) = findprev(query, seq, 
 
 
 """
+    occursin(x::MatchSearchQuery, y::BioSequence)
+
+Return Bool indicating presence of exact match of x in y.
+"""
+Base.occursin(x::MatchSearchQuery, y::BioSequence) = quicksearch(x, y, 1, lastindex(y)) != 0
+
+
+"""
     query_preprocess(type, query)
 
 Preprocesses a search query by building the bloom mask
@@ -75,14 +85,14 @@ function query_preprocess(type::Type{<:ExactSearchQuery{S}}, query::S) where {S<
     fshift = bshift = m
     for i in 1:lastindex(query)
         x = query[i]
-        bloom_mask |= bloom_bits(type, x)
-        if x == last && i < m
+        bloom_mask |= _bloom_bits(type, x)
+        if _ismatch(type, x, last) && i < m
             fshift = m - i
         end
     end
     for i in lastindex(query):-1:1
         x = query[i]
-        if x == first && i > 1
+        if _ismatch(type, x, first) && i > 1
             bshift = i - 1
         end
     end
@@ -105,11 +115,13 @@ function MatchSearchQuery(query::BioSequence)
     return MatchSearchQuery(query, bf, fs, bs)
 end
 
-@inline bloom_mask(q::MatchSearchQuery) = q.bloom_mask
+@inline _bloom_mask(q::MatchSearchQuery) = q.bloom_mask
 
-@inline function bloom_bits(::Type{<:MatchSearchQuery}, x)
+@inline function _bloom_bits(::Type{<:MatchSearchQuery}, x::BioSymbol)
     return (UInt64(1) << (encoded_data(x) & 63))
 end
+
+@inline _ismatch(::Type{<:MatchSearchQuery}, x::BioSymbol, y::BioSymbol) = x == y
 
 # This algorithm is borrowed from base/strings/search.jl, which looks similar to
 # Sunday's Quick Search algorithm.
@@ -143,12 +155,12 @@ function quicksearch(query::MatchSearchQuery, seq, start, stop)
                 return s + 1  # found
             # No match, attempt to rule out next element if end not reached...
             # Essentially, if the next base 
-            elseif s < stop′ && check_bloom_mask(query, seq[s + m + 1])
+            elseif s < stop′ && _check_bloom_mask(query, seq[s + m + 1])
                 s += m + 1
             else
                 s += query.fshift
             end
-        elseif s < stop′ && check_bloom_mask(query, seq[s + m + 1])
+        elseif s < stop′ && _check_bloom_mask(query, seq[s + m + 1])
             s += m + 1
         else
             s += 1
@@ -185,14 +197,12 @@ function quickrsearch(query::MatchSearchQuery, seq, start, stop)
             end
             if i == m + 1
                 return s + 1  # found
-            elseif s > stop′ && check_bloom_mask(query, seq[s])
+            elseif s > stop′ && _check_bloom_mask(query, seq[s])
                 s -= m + 1
-            #elseif isambiguous(seq[s+1])
-            #    s -= 1
             else
                 s -= query.bshift
             end
-        elseif s > stop′ && check_bloom_mask(query, seq[s])
+        elseif s > stop′ && _check_bloom_mask(query, seq[s])
             s -= m + 1
         else
             s -= 1
@@ -203,10 +213,112 @@ function quickrsearch(query::MatchSearchQuery, seq, start, stop)
 end
 
 
-
 """
-    occursin(x::MatchSearchQuery, y::BioSequence)
-
-Return Bool indicating presence of exact match of x in y.
+Query type for compatible sequence search.
 """
-Base.occursin(x::MatchSearchQuery, y::BioSequence) = quicksearch(x, y, 1, lastindex(y)) != 0
+struct CompatibleSearchQuery{S<:BioSequence}
+    seq::S         # query sequence
+    cbits::UInt32  # compatibility bits
+    fshift::Int    # shift length for forward search
+    bshift::Int    # shift length for backward search
+end
+
+
+function CompatibleSearchQuery(query::BioSequence)
+    cbits, fshift, bshift = search_preprocess(query)
+    return CompatibleSearchQuery(query, cbits, fshift, bshift)
+end
+
+@inline bloom_mask(q::CompatibleSearchQuery) = q.bloom_mask
+@inline bloom_bits(::Type{<:CompatibleSearchQuery}, x::NucleicAcid) = encoded_data(x)
+
+@inline function ismatch(::Type{<:CompatibleSearchQuery}, x::BioSymbol, y::BioSymbol)
+    return iscompatible(x, y)
+end
+
+function quicksearch(query::CompatibleSearchQuery, seq, start, stop)
+    pat = query.seq
+    m = length(pat)
+    n = length(seq)
+    stop′ = min(stop, n) - m
+    s::Int = max(start - 1, 0)
+
+    if m == 0  # empty query
+        if s ≤ stop′
+            return s + 1  # found
+        else
+            return 0  # not found
+        end
+    end
+
+    while s ≤ stop′
+        if iscompatible(pat[m], seq[s+m])
+            i = m - 1
+            while i > 0
+                if !iscompatible(pat[i], seq[s+i])
+                    break
+                end
+                i -= 1
+            end
+            if i == 0
+                return s + 1  # found
+            elseif s < stop′ && (query.cbits & compatbits(seq[s+m+1]) == 0)
+                s += m + 1
+            elseif isambiguous(seq[s+m])
+                s += 1
+            else
+                s += query.fshift
+            end
+        elseif s < stop′ && (query.cbits & compatbits(seq[s+m+1]) == 0)
+            s += m + 1
+        else
+            s += 1
+        end
+    end
+
+    return 0  # not found
+end
+
+
+function quickrsearch(query::CompatibleSearchQuery, seq, start, stop)
+    pat = query.seq
+    m = length(pat)
+    n = length(seq)
+    stop′ = max(stop - 1, 0)
+    s::Int = min(start, n) - m
+
+    if m == 0  # empty query
+        if s ≥ stop′
+            return s + 1  # found
+        else
+            return 0  # not found
+        end
+    end
+
+    while s ≥ stop′
+        if iscompatible(pat[1], seq[s+1])
+            i = 2
+            while i < m + 1
+                if !iscompatible(pat[i], seq[s+i])
+                    break
+                end
+                i += 1
+            end
+            if i == m + 1
+                return s + 1  # found
+            elseif s > stop′ && (query.cbits & compatbits(seq[s]) == 0)
+                s -= m + 1
+            elseif isambiguous(seq[s+1])
+                s -= 1
+            else
+                s -= query.bshift
+            end
+        elseif s > stop′ && (query.cbits & compatbits(seq[s]) == 0)
+            s -= m + 1
+        else
+            s -= 1
+        end
+    end
+
+    return 0  # not found
+end
