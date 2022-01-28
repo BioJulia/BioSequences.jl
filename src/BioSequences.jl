8082baa6addf8443270f7036b4d9726d10a165bb
Ciarán O’Mara <ciaran@omara.id.au>
2022-01-29 08:52:03 +1100

    Documentation for `findall` method

--- src/BioSequences.jl
+++ src/BioSequences.jl
@@ -252,13 +252,57 @@ Base.eltype(::Type{<:Search{Q}}) where {Q<:HasRangeEltype} = UnitRange{Int}
 Base.eltype(::Type{<:Search}) = Int
 Base.IteratorSize(::Type{<:Search}) = Base.SizeUnknown()
 
-function Base.findall(pat, seq::BioSequence; overlap::Bool = DEFAULT_OVERLAP)
-    return collect(search(pat, seq; overlap))
+"""
+    findall(pattern, sequence::BioSequence[,rng::UnitRange{Int}]; overlap::Bool=true)
+
+Find all occurrences of `pattern` in `sequence`.
+
+The return value is a vector of ranges of indices where the matching sequences were found.
+If there are no matching sequences, the return is an empty array.
+
+The search is restricted to the specified range when `rng` is set.
+
+With the keyword argument `overlap` set as `true`, the start index for the next search is set to the start of the current match plus one; if set to `false`, the start index for the next search is set to the end of the current match plus one.
+The default value for the keyword argument `overlap` is `true`.
+
+See also [`ExactSearchQuery`](@ref), [`ApproximateSearchQuery`](@ref), [`PWMSearchQuery`](@ref).
+
+# Examples
+```jldoctest
+julia> sequence = dna"ACACACAC"
+8nt DNA Sequence:
+ACACACAC
+
+julia> findall(DNA_A, sequence)
+4-element Vector{Int64}:
+ 1
+ 3
+ 5
+ 7
+
+julia> findall(ExactSearchQuery(dna"ACAC"), sequence)
+3-element Vector{UnitRange{Int64}}:
+ 1:4
+ 3:6
+ 5:8
+
+julia> findall(ExactSearchQuery(dna"ACAC"), sequence; overlap=false)
+2-element Vector{UnitRange{Int64}}:
+ 1:4
+ 5:8
+
+julia> findall(ExactSearchQuery(dna"ACAC"), sequence, 2:7; overlap=false)
+1-element Vector{UnitRange{Int64}}:
+ 3:6
+```
+"""
+function Base.findall(pattern, sequence::BioSequence; overlap::Bool = DEFAULT_OVERLAP)
+    return collect(search(pattern, sequence; overlap))
 end
 
-function Base.findall(pat, seq::BioSequence, rng::UnitRange{Int}; overlap::Bool = DEFAULT_OVERLAP)
-    v = view(seq, rng)
-    itr = search(pat, v; overlap)
+function Base.findall(pattern, sequence::BioSequence, rng::UnitRange{Int}; overlap::Bool = DEFAULT_OVERLAP)
+    v = view(sequence, rng)
+    itr = search(pattern, v; overlap)
     return map(x->parentindices(v)[1][x], itr)
 end
 
