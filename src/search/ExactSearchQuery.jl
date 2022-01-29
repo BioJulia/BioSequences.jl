465a8c66ee0e4d81edde1d2f533bb0e752a2164d
Ciarán O’Mara <ciaran@omara.id.au>
2022-01-30 09:53:00 +1100

    Extract `ExactSearchQuery` query for consistency with other examples

--- src/search/ExactSearchQuery.jl
+++ src/search/ExactSearchQuery.jl
@@ -34,10 +34,12 @@ The default is `isequal`, however, in biology, sometimes we want a more flexible
 comparison to find subsequences of _compatible_ symbols.
 
 ```jldoctest
-julia> findfirst(ExactSearchQuery(dna"CGT", iscompatible), dna"ACNT")  # 'N' matches 'G'
+julia> query = ExactSearchQuery(dna"CGT", iscompatible);
+
+julia> findfirst(query, dna"ACNT")  # 'N' matches 'G'
 2:4
 
-julia> findfirst(ExactSearchQuery(dna"CNT", iscompatible), dna"ACGT")  # 'G' matches 'N'
+julia> findfirst(query, dna"ACGT")  # 'G' matches 'N'
 2:4
 
 julia> occursin(ExactSearchQuery(dna"CNT", iscompatible), dna"ACNT")
@@ -64,17 +66,17 @@ Construct an [`ExactSearchQuery`](@ref) predicate for use with Base find functio
 """
 function ExactSearchQuery(pat::BioSequence, comparator::Function = isequal)
     T = ExactSearchQuery{typeof(comparator),typeof(pat)}
-    
+
     m = length(pat)
     if m == 0
         return T(comparator, pat, UInt64(0), 0, 0)
     end
-    
+
     first = pat[1]
     last = pat[end]
     bloom_mask = zero(UInt64)
     fshift = bshift = m
-    
+
     for i in 1:lastindex(pat)
         x = pat[i]
         bloom_mask |= _bloom_bits(typeof(comparator), x)
@@ -82,14 +84,14 @@ function ExactSearchQuery(pat::BioSequence, comparator::Function = isequal)
             fshift = m - i
         end
     end
-    
+
     for i in lastindex(pat):-1:1
         x = pat[i]
         if comparator(x, first) && i > 1
             bshift = i - 1
         end
     end
-    
+
     return T(comparator, pat, bloom_mask, fshift, bshift)
 end
 
@@ -115,9 +117,9 @@ function quicksearch(query::ExactSearchQuery, seq::BioSequence, start::Integer, 
     comparator = query.comparator
     bloom_mask = query.bloom_mask
     ambig_check = _check_ambiguous(query)
-    
+
     checkeltype(seq, pat)
-    
+
     m = length(pat)
     n = length(seq)
     stop′ = min(stop, n) - m
@@ -164,9 +166,9 @@ function quickrsearch(query::ExactSearchQuery, seq::BioSequence, start::Integer,
     comparator = query.comparator
     bloom_mask = query.bloom_mask
     ambig_check = _check_ambiguous(query)
-    
+
     checkeltype(seq, pat)
-    
+
     m = length(pat)
     n = length(seq)
     stop′ = max(stop - 1, 0)
@@ -253,4 +255,3 @@ Base.findlast(query::ExactSearchQuery, seq::BioSequence) = findprev(query, seq, 
 Return Bool indicating presence of exact match of x in y.
 """
 Base.occursin(x::ExactSearchQuery, y::BioSequence) = quicksearch(x, y, 1, lastindex(y)) != 0
-
