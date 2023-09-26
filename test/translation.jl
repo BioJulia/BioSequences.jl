f64918e27f3f06fe649293c2a6b2d451cf15ec8c
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-09-26 15:42:08 +0200

    Allow translation of LongSubSeq
    This also fixes an issue in the translation code, and adds another constructor
    to LongSubSeq that I thought was missing.

--- test/translation.jl
+++ test/translation.jl
@@ -1,27 +1,57 @@
 @testset "Translation" begin
     # crummy string translation to test against
-    standard_genetic_code_dict = Dict{String,Char}(
-        "AAA" => 'K', "AAC" => 'N', "AAG" => 'K', "AAU" => 'N',
-        "ACA" => 'T', "ACC" => 'T', "ACG" => 'T', "ACU" => 'T',
-        "AGA" => 'R', "AGC" => 'S', "AGG" => 'R', "AGU" => 'S',
-        "AUA" => 'I', "AUC" => 'I', "AUG" => 'M', "AUU" => 'I',
-        "CAA" => 'Q', "CAC" => 'H', "CAG" => 'Q', "CAU" => 'H',
-        "CCA" => 'P', "CCC" => 'P', "CCG" => 'P', "CCU" => 'P',
-        "CGA" => 'R', "CGC" => 'R', "CGG" => 'R', "CGU" => 'R',
-        "CUA" => 'L', "CUC" => 'L', "CUG" => 'L', "CUU" => 'L',
-        "GAA" => 'E', "GAC" => 'D', "GAG" => 'E', "GAU" => 'D',
-        "GCA" => 'A', "GCC" => 'A', "GCG" => 'A', "GCU" => 'A',
-        "GGA" => 'G', "GGC" => 'G', "GGG" => 'G', "GGU" => 'G',
-        "GUA" => 'V', "GUC" => 'V', "GUG" => 'V', "GUU" => 'V',
-        "UAA" => '*', "UAC" => 'Y', "UAG" => '*', "UAU" => 'Y',
-        "UCA" => 'S', "UCC" => 'S', "UCG" => 'S', "UCU" => 'S',
-        "UGA" => '*', "UGC" => 'C', "UGG" => 'W', "UGU" => 'C',
-        "UUA" => 'L', "UUC" => 'F', "UUG" => 'L', "UUU" => 'F',
+    standard_genetic_code_dict = let
+        d_ = Dict{String,Char}(
+            "AAA" => 'K', "AAC" => 'N', "AAG" => 'K', "AAU" => 'N',
+            "ACA" => 'T', "ACC" => 'T', "ACG" => 'T', "ACU" => 'T',
+            "AGA" => 'R', "AGC" => 'S', "AGG" => 'R', "AGU" => 'S',
+            "AUA" => 'I', "AUC" => 'I', "AUG" => 'M', "AUU" => 'I',
+            "CAA" => 'Q', "CAC" => 'H', "CAG" => 'Q', "CAU" => 'H',
+            "CCA" => 'P', "CCC" => 'P', "CCG" => 'P', "CCU" => 'P',
+            "CGA" => 'R', "CGC" => 'R', "CGG" => 'R', "CGU" => 'R',
+            "CUA" => 'L', "CUC" => 'L', "CUG" => 'L', "CUU" => 'L',
+            "GAA" => 'E', "GAC" => 'D', "GAG" => 'E', "GAU" => 'D',
+            "GCA" => 'A', "GCC" => 'A', "GCG" => 'A', "GCU" => 'A',
+            "GGA" => 'G', "GGC" => 'G', "GGG" => 'G', "GGU" => 'G',
+            "GUA" => 'V', "GUC" => 'V', "GUG" => 'V', "GUU" => 'V',
+            "UAA" => '*', "UAC" => 'Y', "UAG" => '*', "UAU" => 'Y',
+            "UCA" => 'S', "UCC" => 'S', "UCG" => 'S', "UCU" => 'S',
+            "UGA" => '*', "UGC" => 'C', "UGG" => 'W', "UGU" => 'C',
+            "UUA" => 'L', "UUC" => 'F', "UUG" => 'L', "UUU" => 'F',
+        )
+        d = Dict{NTuple{3, RNA}, Char}(map(RNA, Tuple(k)) => v for (k, v) in d_)
 
-        # translatable ambiguities in the standard code
-        "CUN" => 'L', "CCN" => 'P', "CGN" => 'R', "ACN" => 'T',
-        "GUN" => 'V', "GCN" => 'A', "GGN" => 'G', "UCN" => 'S'
-    )
+        for (a, b, c) in Iterators.product(ntuple(i -> alphabet(RNA), Val{3}())...)
+            (a == RNA_Gap || b == RNA_Gap || c == RNA_Gap) && continue
+            (a, b, c) ∈ keys(d) && continue
+            possible = ' '
+            for (x, y, z) in Iterators.product(map(BioSequences.UnambiguousRNAs, (a, b, c))...)
+                new_possible = d[(x, y, z)]
+                if possible == ' '
+                    possible = new_possible
+                elseif possible == new_possible
+                    nothing
+                elseif possible ∈ ('J', 'I', 'L') && new_possible ∈ ('I', 'L')
+                    possible = 'J'
+                elseif possible ∈ ('B', 'D', 'N') && new_possible ∈ ('D', 'N')
+                    possible = 'B'
+                elseif possible ∈ ('Z', 'Q', 'E') && new_possible ∈ ('Q', 'E')
+                    possible = 'Z'
+                else
+                    possible = 'X'
+                    break
+                end
+            end
+            d[(a, b, c)] = possible
+        end
+        result = Dict(join(k) => v for (k, v) in d)
+        for (k, v) in result
+            if 'U' in k
+                result[replace(k, 'U'=>'T')] = v
+            end
+        end
+        result
+    end
 
     function string_translate(seq::AbstractString)
         @assert length(seq) % 3 == 0
@@ -30,18 +60,6 @@
             aaseq[div(i, 3) + 1] = standard_genetic_code_dict[seq[i:i+2]]
         end
         return String(aaseq)
-    end
-
-    function check_translate(seq::AbstractString)
-        return string_translate(seq) == String(translate(LongRNA{4}(seq)))
-    end
-
-    aas = aa""
-    global reps = 10
-    for len in [1, 10, 32, 1000, 10000, 100000]
-        @test all(Bool[check_translate(random_translatable_rna(len)) for _ in 1:reps])
-        seq = LongDNA{4}(LongRNA{4}(random_translatable_rna(len)))
-        @test translate!(aas, seq) == translate(seq)
     end
 
     # Basics
@@ -56,6 +74,26 @@
     buf = IOBuffer()
     show(buf, BioSequences.ncbi_trans_table)
     @test !iszero(length(take!(buf)))
+
+    # Test translation values
+    sampler = SamplerWeighted(rna"ACGUMRSVWYHKDBN", [fill(0.2, 4); fill(0.018181818, 10)])
+    for len in [3, 9, 54, 102]
+        for A in [DNAAlphabet{4}(), RNAAlphabet{4}()]
+            for attempt in 1:25
+                seq = randseq(A, sampler, len)
+                @test string(translate(seq)) == string_translate(string(seq))
+                for window in [1:3, 5:13, 11:67]
+                    checkbounds(Bool, eachindex(seq), window) || continue
+                    v = view(seq, window)
+                    @test string(translate(v)) == string_translate(string(v))
+                end
+            end
+        end
+        for A in [DNAAlphabet{2}(), RNAAlphabet{2}()]
+            seq = randseq(A, len)
+            @test string(translate(seq)) == string_translate(string(seq))
+        end
+    end
 
     # ambiguous codons
     @test translate(rna"YUGMGG") == aa"LR"
@@ -84,4 +122,9 @@
 
     # issue #133
     @test translate(rna"GAN") == aa"X"
+
+    # Test views
+    seq = dna"TAGTGCNTAGDACGGGWAAABCCGTAC"
+    @test translate(view(seq, 1:0)) == aa""
+    @test translate(LongSubSeq{RNAAlphabet{4}}(seq, 2:length(seq)-2)) == translate(seq[2:end-2])
 end
