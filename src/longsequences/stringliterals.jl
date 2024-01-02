fa647356f921731bdaa2d8e9f639492196cd4f9b
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-01-02 20:39:05 +0100

    Document string literal macro

--- src/longsequences/stringliterals.jl
+++ src/longsequences/stringliterals.jl
@@ -9,6 +9,40 @@
 
 remove_newlines(s) = replace(s, r"\r|\n" => "")
 
+"""
+    @dna_str(seq, flag="s") -> LongDNA{4}
+
+Create a `LongDNA{4}` sequence at parse time from string `seq`.
+If `flag` is `"s"` ('static', the default), the sequence is created at parse time,
+and inserted directly into the returned expression.
+A static string ought not to be mutated
+Alternatively, if `flag` is `"d"` (dynamic), a new sequence is parsed and created
+whenever the code where is macro is placed is run.
+
+See also: [`@aa_str`](@ref), [`@rna_str`](@ref)
+
+# Examples
+In the example below, the static sequence is created once, at parse time, NOT
+when the function `f` is run. This means it is the _same_  sequence that is
+pushed to repeatedly.
+```jldoctest
+julia> f() = dna"TAG";
+
+julia> string(push!(f(), DNA_A)) # NB: Mutates static string!
+"TAGA"
+
+julia> string(push!(f(), DNA_A))
+"TAGAA"
+
+julia> f() = dna"TAG"d; # dynamically make seq
+
+julia> string(push!(f(), DNA_A))
+"TAGA"
+
+julia> string(push!(f(), DNA_A))
+"TAGA"
+```
+"""
 macro dna_str(seq, flag)
     if flag == "s"
         return LongDNA{4}(remove_newlines(seq))
@@ -24,6 +58,18 @@ macro dna_str(seq)
     return LongDNA{4}(remove_newlines(seq))
 end
 
+"""
+The `LongRNA{4}` equivalent to `@dna_str`
+
+See also: [`@dna_str`](@ref), [`@aa_str`](@ref)
+
+# Examples
+```jldoctest
+julia> rna"UCGUGAUGC"
+9nt RNA Sequence:
+UCGUGAUGC
+```
+"""
 macro rna_str(seq, flag)
     if flag == "s"
         return LongRNA{4}(remove_newlines(seq))
@@ -39,6 +85,19 @@ macro rna_str(seq)
     return LongRNA{4}(remove_newlines(seq))
 end
 
+
+"""
+The `AminoAcidAlphabet` equivalent to `@dna_str`
+
+See also: [`@dna_str`](@ref), [`@rna_str`](@ref)
+
+# Examples
+```jldoctest
+julia> aa"PKLEQC"
+6aa Amino Acid Sequence:
+PKLEQC
+```
+"""
 macro aa_str(seq, flag)
     if flag == "s"
         return LongAA(remove_newlines(seq))
